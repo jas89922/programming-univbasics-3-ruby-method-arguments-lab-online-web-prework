3d70c404f0db0a1c16e42814588e5fd038ab1ebb
Jasmine Kim <jas89922@gmail.com>
2019-09-27 00:23:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
-# def introduction(Jasmine)
+# def introduction(name)
 #  puts "Hi, my name is #{name}."
 #end
 
