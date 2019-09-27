417a6f461dad8a246167d8278e3805d6e653dd98
Jasmine Kim <jas89922@gmail.com>
2019-09-27 00:37:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,11 +8,11 @@
 puts "Hi, my name is {name}."
 end
 
-def introduction_with_language(name = "Jasmine", language = "Ruby")
+def introduction_with_language(name="Jasmine", language="Ruby")
 puts "Hi, my name is {name} and I am learning to program in {language}."
 end
 
-def introduction_with_language_optional(name = "Jasmine", language ="Ruby")
+def introduction_with_language_optional(name="Jasmine", language="Ruby")
 puts "Hi, my name is {name} and I am learning to program in {language}."
 end
 
